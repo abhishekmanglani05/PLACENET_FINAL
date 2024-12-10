015588e18797a7ee5be77b9bd3251823cb45e405
Abhishek Manglani <115579717+abhishekmanglani05@users.noreply.github.com>
2024-12-10 19:03:32 +0530

    Update placenet.py

--- placenet.py
+++ placenet.py
@@ -92,10 +92,10 @@ app = Flask(__name__)
   
 app.secret_key = 'xyzsdfg'
   
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'placenet'
+app.config['MYSQL_HOST'] = 'sql12.freesqldatabase.com'
+app.config['MYSQL_USER'] = 'sql12738991'
+app.config['MYSQL_PASSWORD'] = 'LMQNFU4zpG'
+app.config['MYSQL_DB'] = 'sql12738991'
   
 mysql = MySQL(app)
 
